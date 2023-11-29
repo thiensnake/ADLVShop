08639d21d313cd9049f80058cecdf13c94ed604c
thiensnake <llechithien@gmail.com>
2023-11-30 04:42:04 +0700

    first commit

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//image home admin 
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img src="https://scr.vn/wp-content/uploads/2020/07/h%C3%ACnh-%C4%91%E1%BB%99ng-cho-powerpoint-3.gif" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
